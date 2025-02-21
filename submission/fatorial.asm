f6d46b17df52d9458897731d5778b7f2479e0e5a
Legue2205 <142681474+Legue2205@users.noreply.github.com>
2025-02-21 20:02:03 -0300

    Add files via upload

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -11,14 +11,14 @@ RES K /102
 I K /104
 
 @ /300
-JP /000 ; início da sub-rotina
+LV /000 ; início da sub-rotina
 LV /001 ; AC = 1
 MM RES ; Mem[0102] = AC = 1, N!
 MM I ; Mem[0104] = AC = 1, index do loop
 
 LD N ; AC = N
 SB RES ; AC = N-1
-JN /21E ; if(N-1 < 0) ou seja N = 0, vai 021E, else
+JN /31E ; if(N-1 < 0) ou seja N = 0, vai 021E, else
 
 LD I ; AC = i
 ML RES ; AC *= Mem[0102] --> AC = 1*2*...*i
@@ -28,6 +28,6 @@ AD I ; AC += Mem[0104] --> AC = i+1
 MM I ; Mem[0104] = AC --> i = i+1
 
 SB /110 ; AC -= Mem[0110] --> AC = i-(N+1)
-JN /20E ; if(i-N < 0), repeat
+JN /30E ; if(i-N < 0), repeat
 LD RES ; AC = N!
 RS /300 ; return N! em 102
