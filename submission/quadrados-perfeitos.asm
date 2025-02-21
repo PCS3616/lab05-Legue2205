d7cff32c12fca6673ea46380605c619a849b882d
Legue2205 <142681474+Legue2205@users.noreply.github.com>
2025-02-21 20:45:21 -0300

    Add files via upload

--- submission/quadrados-perfeitos.asm
+++ submission/quadrados-perfeitos.asm
@@ -1,7 +1,7 @@
 @ /000
 START   LV /2 
-        AD & /8
-        MM & /6 ; saves in future position instruction M(9) 10I
+        AD /A
+        MM /A ; saves in future position instruction M(9) 10I
         LD N2
         AD IMPAR
         MM /000 ; instrução alterada +2 cada vez
